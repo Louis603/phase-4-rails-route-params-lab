f49e7752b6a277891d2c80a6d5c915a32f325e66
Louis Y <louisy603@hotmail.com>
2022-06-06 17:58:54 -0400

    commit tree 186c020b1a021d79a8e2e41a193b652663127d4a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.all.where(last_name: params[:name]).or(Student.all.where(first_name: params[:name]))
+    # students = Student.all
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+
+  end
+
+  # def query
+  #   byebug
+  # end
+
+  # Student.all.where(last_name: "Kirby").or(Student.all.where(first_name: "Kirby"))
 end
